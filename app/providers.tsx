1dd16ea3fe059cb1f52ebcfefbbc343355b5c0fc
Jim124 <fengpingan065@gmail.com>
2024-08-20 12:57:07 +0800

    integrate Clerk to the project

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,9 @@
 import { ThemeProvider } from './theme-provider';
+import { Toaster } from '@/components/ui/toaster';
 function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Toaster />
       <ThemeProvider
         attribute='class'
         defaultTheme='system'
--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,4 +1,21 @@
+'use client';
+import { SignOutButton } from '@clerk/nextjs';
+import { useToast } from '../ui/use-toast';
 function SignOutLink() {
-  return <div>SignOutLink</div>;
+  const { toast } = useToast();
+  function handleSignOut() {
+    toast({ description: 'You have been signed out' });
+  }
+  return (
+    <SignOutButton redirectUrl='/'>
+      <button
+        type='button'
+        className='w-full text-left'
+        onClick={handleSignOut}
+      >
+        Logout
+      </button>
+    </SignOutButton>
+  );
 }
 export default SignOutLink;
