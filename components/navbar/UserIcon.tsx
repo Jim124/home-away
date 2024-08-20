3cfc36a3f67b6fa04e3a28bcfb505ea65ee4a0c4
Jim124 <fengpingan065@gmail.com>
2024-08-20 12:01:14 +0800

    modify Navbar and Dark mode

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,4 +1,6 @@
+import { LuUser2 } from 'react-icons/lu';
+
 function UserIcon() {
-  return <div>UserIcon</div>;
+  return <LuUser2 className='w-6 h-6 rounded-full bg-primary' />;
 }
 export default UserIcon;
