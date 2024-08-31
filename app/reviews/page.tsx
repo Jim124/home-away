21cb5deb2ff536ad2ac2c8a8b8e5599550c5a6c8
Jim124 <fengpingan065@gmail.com>
2024-08-31 11:45:42 +0800

    add reviews page

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,4 +1,42 @@
-function ReviewsPage() {
-  return <div>ReviewsPage</div>;
+import EmptyList from '@/components/home/EmptyList';
+import ReviewCard from '@/components/reviews/ReviewCard';
+import Title from '@/components/properties/Title';
+import FormContainer from '@/components/form/FormContainer';
+import { IconButton } from '@/components/form/Buttons';
+
+import {
+  deleteReviewAction,
+  fetchPropertyReviewsByUser,
+} from '@/utils/server-action/reviews';
+
+async function ReviewsPage() {
+  const reviews = await fetchPropertyReviewsByUser();
+  if (reviews.length === 0) return <EmptyList />;
+  return (
+    <section>
+      <Title text='Your reviews' />
+      <div className='grid md:grid-cols-2 gap-8 mt-4'>
+        {reviews.map((review) => {
+          const { rating, comment } = review;
+          const { name, image } = review.property;
+          const reviewInfo = { comment, rating, name, image };
+          return (
+            <ReviewCard key={review.id} reviewInfo={reviewInfo}>
+              <DeleteReview reviewId={review.id} />
+            </ReviewCard>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 export default ReviewsPage;
+
+const DeleteReview = ({ reviewId }: { reviewId: string }) => {
+  const deleteReview = deleteReviewAction.bind(null, { reviewId });
+  return (
+    <FormContainer action={deleteReview}>
+      <IconButton actionType='delete' />
+    </FormContainer>
+  );
+};
